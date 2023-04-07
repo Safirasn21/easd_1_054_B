9568b9655b9d63e31526ce3240d3402370268164
safirasn21 <safirasn021@gmai.com>
2023-04-07 10:49:52 +0700

    procedure untuk menggabungkan 2 array yang telah diurutkan

--- Mergesort/Mergesort.cpp
+++ Mergesort/Mergesort.cpp
@@ -3,3 +3,8 @@ using namespace std;
 
 const int MAX_SIZE = 14; // sumbu dari 54 + 20 - 2 x 15 + 10 + 20
 int rara[MAX_SIZE];
+
+void merge(int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int L[10], R[10];
